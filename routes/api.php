509a1057a03f3be665dc4efa0e937852fff0ea05
Lucas Silva <lucasalmeidab@gmail.com>
2021-04-09 09:38:44 -0300

    cleanup routes

--- routes/api.php
+++ routes/api.php
@@ -22,19 +22,10 @@ Route::post('/login', LoginController::class);
 
 Route::get('/news', [NewsController::class, 'index']);
 
-Route::post('/news', [NewsController::class, 'store'])->middleware('auth:sanctum');
-Route::put('/news/{news}', [NewsController::class, 'update'])->middleware('auth:sanctum');
-Route::delete('/news/{news}', [NewsController::class, 'destroy'])->middleware('auth:sanctum');
+Route::middleware('auth:sanctum')->group(function(){
+    Route::resource('/news', NewsController::class)->except('index');
+    Route::resource('/events', EventController::class);
 
-Route::post('/news/{news}/comments', [NewsCommentController::class, 'store'])->middleware('auth:sanctum');
-Route::delete('/news/{news}/comments/{comment}', [NewsCommentController::class, 'destroy'])->middleware('auth:sanctum');
-
-Route::get('/events', [EventController::class, 'index'])->middleware('auth:sanctum');
-Route::post('/events', [EventController::class, 'store'])->middleware('auth:sanctum');
-Route::put('/events/{event}', [EventController::class, 'update'])->middleware('auth:sanctum');
-Route::delete('/events/{event}', [EventController::class, 'destroy'])->middleware('auth:sanctum');
-
-Route::post('/events/{event}/comments', [EventCommentController::class, 'store'])
-    ->middleware('auth:sanctum');
-Route::delete('/events/{event}/comments/{comment}', [EventCommentController::class, 'destroy'])
-    ->middleware('auth:sanctum');
+    Route::resource('news.comments', NewsCommentController::class);
+    Route::resource('events.comments', EventCommentController::class);
+});
